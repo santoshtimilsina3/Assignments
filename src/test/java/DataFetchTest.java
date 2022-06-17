cad301673735745be04810eefc874623bc642579
santoshtimilsina3 <santos.timil@gmail.com>
2022-06-18 00:18:39 +0545

    test case complete refactor required

--- src/test/java/DataFetchTest.java
+++ src/test/java/DataFetchTest.java
@@ -11,44 +11,66 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 
-
 public class DataFetchTest {
 
     @InjectMocks
     JdbcOperation jdbc = new JdbcOperation();
 
     @Test
-    public void onlyAccountRecords() throws FileNotFoundException, IOException {
+    public void receiptOnlyRecords() throws FileNotFoundException, IOException {
+
         //get data from database
-        List<ArrayList> list = jdbc.onlyAccountRecords();
-
-        //Read file from csv file
-        FileReader fr = new FileReader("/home/sagar/NetBeansProjects/Assignments/accountonly.csv");
-        BufferedReader br = new BufferedReader(fr);
-        String line;
-        List<ArrayList> lists = new ArrayList<>();
-        while ((line = br.readLine()) != null) {
-            System.out.println(line.length());
+        List<ArrayList> list = jdbc.onlyReceiptRecords();
+        Object databaseData[][] = new Object[3][4];
+        for (int i = 0; i < list.size(); i++) {
+            ArrayList<Object> eachList = list.get(i);
+            for (int j = 0; j < eachList.size(); j++) {
+                databaseData[i][j] = eachList.get(j);
+            }
         }
-        System.out.println(lists+"lists");
-        br.close();
-        File file = new File("/home/sagar/NetBeansProjects/Assignments/accountonly.csv");
+
+        String line = "";
+        String splitBy = ",";
+
+        //parsing a CSV file into BufferedReader class constructor  
+        String[][] fileData = new String[3][4];
+        BufferedReader br = new BufferedReader(new FileReader("/home/sagar/NetBeansProjects/Assignments/receiptonly.csv"));
+        int i = 0;
+        while ((line = br.readLine()) != null) {
+            String[] employee = line.split(splitBy);
+            for (int j = 0; j < employee.length; j++) {
+                fileData[i][j] = employee[j];
+            }
+            i++;
+        }
+
+        //Test if the file exits
+        File file = new File("/home/sagar/NetBeansProjects/Assignments/receiptonly.csv");
         Assert.assertTrue(file.exists());
+       //test if the data of database and file is same
+        for (int a = 0; a < 3; i++) {
+            for (int b = 0; b < 4; b++) {
+                Assert.assertEquals(databaseData[a][b].toString(), fileData[a][b]);
+            }
+        }
     }
-
 }
-////File file=new File("Demo.txt");    //creates a new file instance  
-//FileReader fr=new FileReader(file);   //reads the file  
-//BufferedReader br=new BufferedReader(fr);  //creates a buffering character input stream  
-//StringBuffer sb=new StringBuffer();    //constructs a string buffer with no characters  
-//String line;  
-//while((line=br.readLine())!=null)  
-//{  
-//sb.append(line);      //appends line to string buffer  
-//sb.append("\n");     //line feed   
-//}  
-//fr.close();    //closes 
 
+//Read file from csv file
+//        FileReader fr = new FileReader("/home/sagar/NetBeansProjects/Assignments/receiptonly.csv");
+//        BufferedReader br = new BufferedReader(fr);
+//        String fileData[][] = new String[3][4];
+//        String line;
+//        while ((line = br.readLine()) != null) {
+//            System.out.println(line + "before split array");
+//            // line = line.substring(0, line.length() - 1);
+//            System.out.println(line + "char array");
+//            //    lists.add(line);
+//        }
+//
+//        }
+//       for (int i = 0; i < lists.size(); i++) {
+//
 //    1. call
 //            2. read file
 //                    3. assert isfile created
